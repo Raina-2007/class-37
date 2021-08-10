130c39e12e874d4f83c69aaeb9a392e9dc72d324
Raina-2007 <75320146+Raina-2007@users.noreply.github.com>
2021-08-10 18:11:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var canvas;
+var backgroundImage, car1_img , car2_img , track;
+var bgImg;
+var database;
+var form, player;
+var playerCount , gameState;
+var car1,car2 ,cars,allPlayers;
+
+function preload() {
+  backgroundImage = loadImage("./assets/background.png");
+  car1_img = loadImage("./assets/car1.png");
+  car2_img = loadImage("./assets/car2.png");
+  track = loadImage("./assets/track.jpg");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+
+}
+
+function draw() {
+  background(backgroundImage);
+  if(playerCount === 2){
+    game.update(1);
+  }
+  if(gameState === 1){
+    game.play();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
